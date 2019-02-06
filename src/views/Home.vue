ae1be097a6c857c50094f28a16d609506a27d0cc
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-06 17:12:12 -0500

    #6 - Breakpoints & Visibility

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,9 @@
 <template lang="pug">
   .home
     h1 Homepage
-    v-btn.pink.white--text Click me
-    v-btn(depressed, color="pink") Click me
-    v-btn(flat, color="pink") Click me
-
-    v-btn.pink.white--text(depressed)
-      v-icon(left) email
-      span Email me
-
-    v-btn.pink.white--text(depressed, small)
-      v-icon(left, small) email
-      span Email me
-    
-    v-btn.pink.white--text(depressed, large)
-      span Email me
-      v-icon(right, large) email
-
-    v-btn(fab, depressed, small, dark, color="purple")
-      v-icon favorite
+    //- v-btn.hidden-md-and-down Click me
+    //- v-btn.hidden-md-and-up Click me
+    v-btn.hidden-sm-only Click me
 </template>
 
 <script>
