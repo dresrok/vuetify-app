0cc30266a77006b40d1b6f06fe2b2c6904564f3a
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-06 16:29:20 -0500

    #3 - Vuetify Basics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,9 @@
-<template>
-  <HelloWorld />
+<template lang="pug">
+  .home
+    h1 Homepage
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
-    components: {
-      HelloWorld
-    }
   }
 </script>
