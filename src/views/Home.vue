c25f53ae8c0538a6da9d3107ec3dc947c6b27979
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-06 17:02:26 -0500

    #5 - Button & Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template lang="pug">
   .home
     h1 Homepage
-    p.red.white--text Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    p.pink.lighten-4.red--text.text--darken-4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    h1.display-4 Massive Display
-    h4.display-1 Smaller Display
-    p.headline This is a headline
-    p.subheading.font-weight-bold This s a subheading
-    p.caption This is a caption
+    v-btn.pink.white--text Click me
+    v-btn(depressed, color="pink") Click me
+    v-btn(flat, color="pink") Click me
+
+    v-btn.pink.white--text(depressed)
+      v-icon(left) email
+      span Email me
+
+    v-btn.pink.white--text(depressed, small)
+      v-icon(left, small) email
+      span Email me
+    
+    v-btn.pink.white--text(depressed, large)
+      span Email me
+      v-icon(right, large) email
+
+    v-btn(fab, depressed, small, dark, color="purple")
+      v-icon favorite
 </template>
 
 <script>
