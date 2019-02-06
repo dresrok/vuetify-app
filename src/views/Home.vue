ee339d77f57e3ca0746e0e98f3e77df2d31d413d
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-06 16:46:56 -0500

    #4 - Text & Colours

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template lang="pug">
   .home
     h1 Homepage
+    p.red.white--text Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    p.pink.lighten-4.red--text.text--darken-4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    h1.display-4 Massive Display
+    h4.display-1 Smaller Display
+    p.headline This is a headline
+    p.subheading.font-weight-bold This s a subheading
+    p.caption This is a caption
 </template>
 
 <script>
-  export default {
-  }
+export default {
+  
+}
 </script>
