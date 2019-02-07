358bba8dea45b4dce533baa36f86d37521667c74
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-07 16:46:53 -0500

    #15 - Dummy Project Data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,24 +2,46 @@
   .dashboard
     h1.subheading.grey--text Dashboard
     v-container.my-5
-      v-card.pa-3(flat)
-        v-layout(row, wrap)
+      v-card(flat, v-for="project in projects" :key="project.title")
+        v-layout(row, wrap, :class="`pa-3 project ${project.status}`")
           v-flex(xs12, md6)
             .caption.grey--text Project Title
-            div Create a new website
+            div {{ project.title }}
           v-flex(xs6, sm4, md2)
             .caption.grey--text Person
-            div The Net Ninja
+            div {{ project.person }}
           v-flex(xs6, sm4, md2)
             .caption.grey--text Due by
-            div 1st Jan 2019
+            div {{ project.due }}
           v-flex(xs2, sm4, md2)
             .caption.grey--text Status
-            div ongoing
+            div {{ project.status }}
+        v-divider
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="stylus">
+.project
+  &.complete
+    border-left 4px solid #3CD1C2
+  &.ongoing
+    border-left: 4px solid orange
+  &.overdue
+    border-left 4px solid tomato
+
+</style>
+
