343947a901f9a5452fa31effb2b63051bfde076a
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-07 16:33:12 -0500

    #14 - Grid System (part 2)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,20 +2,20 @@
   .dashboard
     h1.subheading.grey--text Dashboard
     v-container.my-5
-      v-layout(row, wrap)
-        v-flex(xs12, md6)
-          v-btn.primary(outline, block) 1
-        v-flex(xs4, md2)
-          v-btn.primary(outline, block) 2
-        v-flex(xs4, md2)
-          v-btn.primary(outline, block) 2
-        v-flex(xs4, md2)
-          v-btn.primary(outline, block) 2
-      v-layout(row, wrap, justify-space-around)
-        v-flex(xs4, md3)
-          v-btn.success(outline, block) 1
-        v-flex(xs4, md3)
-          v-btn.success(outline, block) 2
+      v-card.pa-3(flat)
+        v-layout(row, wrap)
+          v-flex(xs12, md6)
+            .caption.grey--text Project Title
+            div Create a new website
+          v-flex(xs6, sm4, md2)
+            .caption.grey--text Person
+            div The Net Ninja
+          v-flex(xs6, sm4, md2)
+            .caption.grey--text Due by
+            div 1st Jan 2019
+          v-flex(xs2, sm4, md2)
+            .caption.grey--text Status
+            div ongoing
 </template>
 
 <script>
