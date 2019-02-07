382b997414dbd2a80884c8ec1bcb76504a42beda
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-07 16:24:02 -0500

    #13 - The Grid System (part 1)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,21 @@
 <template lang="pug">
   .dashboard
     h1.subheading.grey--text Dashboard
-    v-container.my-5 content
+    v-container.my-5
+      v-layout(row, wrap)
+        v-flex(xs12, md6)
+          v-btn.primary(outline, block) 1
+        v-flex(xs4, md2)
+          v-btn.primary(outline, block) 2
+        v-flex(xs4, md2)
+          v-btn.primary(outline, block) 2
+        v-flex(xs4, md2)
+          v-btn.primary(outline, block) 2
+      v-layout(row, wrap, justify-space-around)
+        v-flex(xs4, md3)
+          v-btn.success(outline, block) 1
+        v-flex(xs4, md3)
+          v-btn.success(outline, block) 2
 </template>
 
 <script>
