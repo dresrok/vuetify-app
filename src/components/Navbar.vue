4d5101a3b24bb1e3328a9c1a23482e27a7726fb7
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-07 14:53:07 -0500

    #8 - Navigation Drawers

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   nav
     v-toolbar(flat, app)
+      v-toolbar-side-icon.grey--text(@click="drawer = !drawer")
       v-toolbar-title.text-uppercase.grey--text
         span.font-weight-light Todo
         span Ninja
@@ -8,10 +9,17 @@
       v-btn(flat, color="grey")
         span Sign Out
         v-icon(right) exit_to_app
+
+    v-navigation-drawer.indigo(app v-model="drawer")
+      p Test
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      drawer: false
+    }
+  },
 }
 </script>
