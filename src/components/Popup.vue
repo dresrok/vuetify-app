c6c5e25a887771d789a27938722814b701b31e4b
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-08 15:47:56 -0500

    #24 - Form Basics

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -4,10 +4,25 @@
     v-card
       v-card-title
         h2 Add a New Project
+      v-card-text
+        v-form.px-3
+          v-text-field(label="Title", v-model="title", prepend-icon="folder")
+          v-textarea(label="Information", v-model="content", prepend-icon="edit")
+          v-btn.success.mx-0(flat, @click="submit") Add project
 </template>
 
 <script>
 export default {
-  
+  data () {
+    return {
+      title: '',
+      content: ''
+    }
+  },
+  methods: {
+    submit () {
+      console.log(this.title, this.content)
+    }
+  }
 }
 </script>
