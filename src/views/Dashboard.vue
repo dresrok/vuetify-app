930af7206710342c64dea781832502198e43735a
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-02-11 00:44:18 -0500

    #31 - Firestore Real-time Listeners

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -13,7 +13,7 @@
             v-icon(left, small) person
             span.caption.text-lowercase By person
           span Sort projects by person
-      v-card(flat, v-for="project in projects" :key="project.title")
+      v-card(flat, v-for="project in projects" :key="project.id")
         v-layout(row, wrap, :class="`pa-3 project ${project.status}`")
           v-flex(xs12, md6)
             .caption.grey--text Project Title
@@ -31,22 +31,32 @@
 </template>
 
 <script>
+import db from '@/fb'
+
 export default {
   data() {
     return {
-      projects: [
-        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-      ]
+      projects: []
     }
+  },
+  created () {
+    db.collection('projects').onSnapshot(res => {
+      const changes = res.docChanges()
+      changes.forEach(change => {
+        if (change.type === 'added') {
+          this.projects.push({
+            ...change.doc.data(),
+            id: change.doc.id
+          })
+        }
+      })
+    })
   },
   methods: {
     sortBy (prop) {
       this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
     }
-  },
+  }
 }
 </script>
 
